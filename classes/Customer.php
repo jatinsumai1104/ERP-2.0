c4de8e8f4989f60d58a5137925f92f6028fed84b
Varun-Jethanandani <varunjhd@gmail.com>
2020-01-15 02:28:45 +0530

    Manage Customer

--- classes/Customer.php
+++ classes/Customer.php
@@ -2,25 +2,14 @@
 class Customer
 {
     private $table="customers";
-    private $id;
-    private $first_name;
-    private $last_name;
-    private $gst_no;
-    private $phone_no;
-    private $email_id;
-    private $gender;
-    private $deleted;
-    private $created_at;
-    private $updated_at;
     private $db;
     
     public function __construct(Database $db){
         $this->db = $db;
-        $this->db = $this->db->table($this->table);
     }
 
     function readAllCustomers(){
-        $customerData =  $this->db->readData();
+        $customerData =  $this->db->readData($this->table);
         return $customerData;
     }
 
--- views/pages/customers/manage-customer.php
+++ views/pages/customers/manage-customer.php
@@ -79,8 +79,8 @@ require_once('../../includes/header.php');
                       <td><?php echo $customer_details[$i]['phone_no'];?></td>
                       <td><?php echo $customer_details[$i]['email_id'];?></td>
                       <td><?php echo $customer_details[$i]['gender'];?></td>
-                      <td><button type="button" id="edit" class="btn btn-primary btn-block"><i class="fas fa-pencil-alt"></i> Edit</button></td>
-                      <td><button type="button" id="delete" class="btn btn-danger btn-block"><i class="far fa-trash-alt"></i> Delete</button></td>
+                      <td><button type="button" id=<?php echo $customer_details[$i]['id'] ?> class="btn btn-primary btn-block edit"><i class="fas fa-pencil-alt"></i> Edit</button></td>
+                      <td><button type="button" id=<?php echo $customer_details[$i]['id'] ?>  class="btn btn-danger btn-block delete"><i class="far fa-trash-alt"></i> Delete</button></td>
                     </tr>
                     <?php
                     }
@@ -88,15 +88,36 @@ require_once('../../includes/header.php');
                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Open modal for @mdo</button>
 
                     <!-- BEGIN EDIT MODAL -->
-                        
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Open modal for @mdo</button>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form>
+                            <div class="form-group">
+                                <label for="recipient-name" class="col-form-label">Recipient:</label>
+                                <input type="text" class="form-control" id="recipient-name">
+                            </div>
+                            <div class="form-group">
+                                <label for="message-text" class="col-form-label">Message:</label>
+                                <textarea class="form-control" id="message-text"></textarea>
+                            </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Send message</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
                     <!-- END OF EDIT MODAL -->
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Send message</button>
-      </div>
-    </div>
-  </div>
-</div>
                   </tbody>
                 </table>
               </div>
@@ -126,7 +147,6 @@ require_once('../../includes/header.php');
 <?php
   require_once('../../includes/scripts.php');
 ?>
-<script src="../../../assets/js/datatables.js"></script>
 </body>
 
 </html>
