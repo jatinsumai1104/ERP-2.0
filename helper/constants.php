f486ef6cef6044199bd108f4bb83728d73923eb6
jatinsumai1104 <jatinsumai1104@gmail.com>
2020-01-14 19:01:43 +0530

    UI Pages Splitting

---
+++ helper/constants.php
@@ -0,0 +1,10 @@
+
+<?php 
+
+define("BASEURL", "http://localhost/oop-php-erp/");
+define("BASEASSETS", BASEURL."assets/");
+define("BASEPAGES", BASEURL."views/pages/");
+
+?>
+
+
--- views/pages/register.php
+++ views/pages/register.php
@@ -1,24 +1,13 @@
+<?php
+  require_once('../../helper/constants.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>SB Admin 2 - Register</title>
-
-  <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles for this template-->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
-</head>
+<!-- Header containing all Links -->
+<?php
+  require_once('../includes/header.php');
+?>
 
 <body class="bg-gradient-primary">
 
@@ -67,10 +56,10 @@
               </form>
               <hr>
               <div class="text-center">
-                <a class="small" href="forgot-password.html">Forgot Password?</a>
+                <a class="small" href="<?php echo BASEPAGES?>forgot-password.php">Forgot Password?</a>
               </div>
               <div class="text-center">
-                <a class="small" href="login.html">Already have an account? Login!</a>
+                <a class="small" href="<?php echo BASEPAGES?>login.php">Already have an account? Login!</a>
               </div>
             </div>
           </div>
