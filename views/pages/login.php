8dc805e5d9414458c62c6c9a82efb37733d347ac
n7aakash <t2lassi12345@gmail.com>
2020-01-14 23:26:55 +0530

    some bug fixes

--- views/pages/login.php
+++ views/pages/login.php
@@ -1,8 +1,6 @@
 <?php
 
 
-
-require_once ("../../helper/init.php");
 require_once('../../helper/constants.php');
 ?>
 <!DOCTYPE html>
@@ -32,12 +30,12 @@ require_once('../../helper/constants.php');
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                   </div>
-                  <form class="user" action="../../helper/routing.php">
+                  <form method="post" class="user" action="../../helper/routing.php">
                     <div class="form-group">
-                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                      <input type="email" name="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                      <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
                     </div>
                     <div class="form-group">
                       <div class="custom-control custom-checkbox small">
@@ -45,9 +43,9 @@ require_once('../../helper/constants.php');
                         <label class="custom-control-label" for="customCheck">Remember Me</label>
                       </div>
                     </div>
-                    <a href="index.html" class="btn btn-primary btn-user btn-block">
+                    <button type="submit" name="login_details" class="btn btn-primary btn-user btn-block">
                       Login
-                    </a>
+                    </button>
                     <hr>
                     <a href="index.html" class="btn btn-google btn-user btn-block">
                       <i class="fab fa-google fa-fw"></i> Login with Google
