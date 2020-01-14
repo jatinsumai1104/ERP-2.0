84af09940bdb2816df0a7c3343bcd0cc9eca6de2
jatinsumai1104 <jatinsumai1104@gmail.com>
2020-01-15 00:32:59 +0530

    manage-product page commited

---
+++ classes/Product.class.php
@@ -0,0 +1,16 @@
+<?php
+  class Product{
+    protected $db ;
+    public function __construct(Database $db){
+      $this->db = $db;
+    }
+
+
+    public function getDataForDataTables(){
+      $query = "SELECT p.id as product_id, p.name as product_name, p.specification, psr.selling_rate, p.eoq_level, p.danger_level,cat.name, GROUP_CONCAT(CONCAT(s.first_name,' ', s.last_name)) as supplier_name from products as p INNER JOIN product_supplier as ps ON p.id = ps.product_id INNER JOIN suppliers as s ON ps.supplier_id = s.id INNER JOIN category as cat ON p.category_id = cat.id INNER JOIN products_selling_rate as psr ON p.id = psr.product_id GROUP BY p.id ORDER BY p.id ASC";
+      
+      $res = $db->query($query);
+      var_dump($res);
+    }
+  }
+?>
