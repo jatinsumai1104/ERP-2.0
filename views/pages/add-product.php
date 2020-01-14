774477883cba094e049d7e87ea94ff5e2090ed04
jatinsumai1104 <jatinsumai1104@gmail.com>
2020-01-14 22:05:12 +0530

    Updating add-product UI

--- views/pages/add-product.php
+++ views/pages/add-product.php
@@ -39,88 +39,64 @@ require_once('../includes/header.php');
 
         <!-- Page Heading -->
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
-          <h1 class="h3 mb-0 text-gray-800"><i class="fas fa-plus"></i> Add Product</h1>
+          <h1 class="h3 mb-0 text-gray-800">Product</h1>
           <a href="<?echo BASEPAGES?>manage-product.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-list-ul fa-sm text-white-75"></i> Manage Product </a>
         </div>
 
         <!-- Content Row -->
+        <div class="container-fluid">
         <div class="row">
-
-          <!-- Earnings (Monthly) Card Example -->
-          <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-              <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                  <div class="col mr-2">
-                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Earnings (Monthly)</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-                  </div>
-                  <div class="col-auto">
-                    <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                  </div>
-                </div>
-              </div>
+          <div class="col-md-12">
+          <div class="card shadow mb-4">
+            <!-- Card Header - Dropdown -->
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+              <h6 class="m-0 font-weight-bold text-primary"><i class="fas fa-plus"></i> Add Product</h6>
             </div>
-          </div>
-
-          <!-- Earnings (Monthly) Card Example -->
-          <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
+            <!-- Card Body -->
+            <form action="" method="POST">
               <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                  <div class="col mr-2">
-                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="">Product Name</label>
+                    <input type="text"
+                      class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
                   </div>
-                  <div class="col-auto">
-                    <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                  <div class="form-group">
+                    <label for="">Product Name</label>
+                    <input type="text"
+                      class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
                   </div>
+                  <div class="form-group">
+                    <label for="">Product Name</label>
+                    <input type="text"
+                      class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+                  </div>
+                  
                 </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- Earnings (Monthly) Card Example -->
-          <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-info shadow h-100 py-2">
-              <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                  <div class="col mr-2">
-                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
-                    <div class="row no-gutters align-items-center">
-                      <div class="col-auto">
-                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                      </div>
-                      <div class="col">
-                        <div class="progress progress-sm mr-2">
-                          <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                      </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="">Product Name</label>
+                      <input type="text"
+                        class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
                     </div>
-                  </div>
-                  <div class="col-auto">
-                    <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                    <div class="form-group">
+                      <label for="">Product Name</label>
+                      <input type="text"
+                        class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+                    </div>
+                    <div class="form-group">
+                      <label for="">Product Name</label>
+                      <input type="text"
+                        class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+                    </div>
+                    
                   </div>
                 </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
               </div>
-            </div>
+            </form>
           </div>
-
-          <!-- Pending Requests Card Example -->
-          <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-warning shadow h-100 py-2">
-              <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                  <div class="col mr-2">
-                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                  </div>
-                  <div class="col-auto">
-                    <i class="fas fa-comments fa-2x text-gray-300"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
 
