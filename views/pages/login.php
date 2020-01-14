8966a1dc380506297a7573f0930349e2a40c7389
n7aakash <t2lassi12345@gmail.com>
2020-01-14 22:48:28 +0530

    deleted unnecessary files

--- views/pages/login.php
+++ views/pages/login.php
@@ -1,5 +1,9 @@
 <?php
-  require_once('../../helper/constants.php');
+
+
+
+require_once ("../../helper/init.php");
+require_once('../../helper/constants.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,7 +32,7 @@
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                   </div>
-                  <form class="user">
+                  <form class="user" action="../../helper/routing.php">
                     <div class="form-group">
                       <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
                     </div>
